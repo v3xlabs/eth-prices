ec80f846af3c4e307978240e20d4f21c1fd69c2f
Luc <luc@lucemans.nl>
2026-03-23 22:49:40 +0100

    xz

--- pkg/src/lib.rs
+++ pkg/src/lib.rs
@@ -35,14 +35,14 @@ Routing is currently in-progress and will be available in a future release.
 You can find more examples in the [examples](https://github.com/v3xlabs/eth-prices/tree/master/examples) directory.
 */
 
+pub mod builder;
 pub mod config;
 pub mod quoter;
 pub mod router;
 pub mod token;
 
-// #[cfg(target_arch = "wasm32")]
+#[cfg(feature = "wasm-bindings")]
 pub mod js;
 
-// Utilities for testing and development
 #[cfg(test)]
 pub mod tests;
